c9ee9544ad9aa162355bfc0c5d932dd0dc4f9fe6
JsmediaGP <jgraphics73@gmail.com>
2024-05-23 08:49:06 +0100

    Login and registration done

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    //
+
+    public function register(Request $request){
+
+        // Validate request data
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]
+        // ,[
+        //     'name.required' => 'Your name is required.',
+        //     'email.unique' => 'An account with this email address already exists.',
+
+        // ]
+         );
+
+        // Create user
+        $user = \App\Models\User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        // Return response
+        return response()->json(['message' => 'Registration successful'], 201);
+
+    }
+
+
+    public function login(Request $request){
+         // Validate request data
+         $credentials = $request->only('email', 'password');
+
+         if (!Auth::attempt($credentials)) {
+             throw ValidationException::withMessages([
+                 'email' => ['The provided credentials do not match our records.'],
+             ]);
+         }
+ 
+         // Generate token
+         $token = $request->user()->createToken('railway-token')->plainTextToken;
+ 
+         // Return response
+         return response()->json(['access_token' => $token], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    
+    // Admin (ITCC) routes
+    Route::prefix('admin')->group(function () {
+       
+    });
+
+});
+
+
