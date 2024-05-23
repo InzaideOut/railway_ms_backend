e633d5a6d48588688ae82d104e75ebf51a495f2c
JsmediaGP <jgraphics73@gmail.com>
2024-05-23 02:43:53 +0100

    Install API, Database, and some models

---
+++ app/Models/TrainUsers.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TrainUsers extends Model
+{
+    use HasFactory;
+}
